89ec1d9dab721a89ff48ee1f4e50b1ad956740c6
Niteesh Kulhari <nkulhari96@gmail.com>
2024-01-27 01:15:46 -0500

    code

--- week-7/7.1/src/App.jsx
+++ week-7/7.1/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Landing } from './components/Landing'
+//import { Landing } from './components/Landing'
 import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
+//import { Dashboard } from './components/Dashboard'
+const Dashboard = lazy(()=> import ('./components/Dashboard'))
+const Landing =  lazy(()=> import('./components/Landing')) 
 
 function App() {
   
@@ -16,8 +18,8 @@ function App() {
     <BrowserRouter>
     <Appbar/>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/" element={<Landing/>}/>
+        <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard/></Suspense>}/>
+        <Route path="/" element={<Suspense fallback={"loading..."}><Landing/></Suspense>}/>
       </Routes>
     </BrowserRouter>
   
